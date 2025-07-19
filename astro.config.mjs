0baafb632d8391480583ca8139b62aaa496d8fb1
lukemcdonald <thelukemcdonald@gmail.com>
2025-07-19 10:00:46 -0500

    feat: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  adapter: netlify()
 });
