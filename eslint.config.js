0371d31565994b805e9fcedc7865e1c8d0731f66
lukemcdonald <thelukemcdonald@gmail.com>
2025-07-19 17:03:14 -0500

    feat: add linting and format scripts

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    rules: {
+      'no-console': 'warn'
+    }
+  },
+  {
+    ignores: [
+      'dist/**',
+      '.astro/**',
+    ],
+  },
+]
