f264c9bd527be5b3c4490fd0fd0d375728ebfd46
Luke McDonald <lukemcdonald@users.noreply.github.com>
2025-09-07 08:23:22 -0500

    Implement public google site verification (#34)

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,14 +1,14 @@
 import { envField } from 'astro/config'
 
 export const ENV_SCHEMA = {
+  GOOGLE_SITE_VERIFICATION: envField.string({
+    access: 'public',
+    context: 'client',
+    optional: true,
+  }),
   POSTHOG_API_KEY: envField.string({
     access: 'public',
     context: 'client',
     default: 'phc_uOQ6lcaumKxM0PL2tuStM1uioEePx2JfFB7680cpHQF',
-  }),
-  PUBLIC_GOOGLE_SITE_VERIFICATION: envField.string({
-    access: 'public',
-    context: 'client',
-    optional: true,
   }),
 }
