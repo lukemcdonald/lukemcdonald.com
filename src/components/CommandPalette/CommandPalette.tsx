c6adb8bd50561826297a47d2aa2a8dcd26eeceaf
Luke McDonald <thelukemcdonald@gmail.com>
2026-08-28 14:45:45 -0500

    fix: hide cmd+k on mobile for now

--- src/components/CommandPalette/CommandPalette.tsx
+++ src/components/CommandPalette/CommandPalette.tsx
@@ -60,7 +60,7 @@ export default function CommandPalette({ navigationItems = [] }: CommandPaletteP
     <>
       <button
         aria-label="Open command palette"
-        className="rounded-md bg-black/0 px-2.5 py-2 text-base font-semibold tracking-wide text-primary-900 uppercase hover:bg-black/5 focus:outline-hidden sm:px-3"
+        className="hidden rounded-md bg-black/0 px-2.5 py-2 text-base font-semibold tracking-wide text-primary-900 uppercase hover:bg-black/5 focus:outline-hidden sm:px-3 [@media(hover:hover)_and_(pointer:fine)]:inline-flex"
         type="button"
         onClick={() => setIsOpen(true)}
       >
